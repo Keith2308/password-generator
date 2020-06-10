62505a83a1d76ad498ed9a3987e13a17644458fb
Keith <ongkeeaik@gmail.com>
2020-06-10 15:55:14 +0800

    display background color

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -9,15 +9,36 @@ const Display = () => {
   const [rangeValue, setRange] = useState();
   const [passwordProps, setPasswordProps] = useState();
 
+  let pwdDescription = "";
+
   const generateNewPassword = () => {
     const pwd = generatePassword(passwordProps, rangeValue);
     setPassword(pwd);
   };
 
+  const setBackgroundColor = (password) => {
+    if (password && password.length === 1 && password.length <= 5) {
+      pwdDescription = "Bad Password";
+      return "#cb473e";
+    } else if (password && password.length >= 6 && password.length <= 10) {
+      pwdDescription = "Weak Password";
+      return "#f07d58";
+    } else if (password && password.length >= 10) {
+      pwdDescription = "Strong Password";
+      return "#55a95d";
+    } else {
+      pwdDescription = "Bad Password";
+      return "#cb473e";
+    }
+  };
+
   return (
     <>
       <div className="row">
-        <div className="col-md-12 password-display-container">
+        <div
+          className="col-md-12 password-display-container"
+          style={{ backgroundColor: setBackgroundColor(password) }}
+        >
           <div style={{ width: "100%" }}>
             <div className="password-display">
               <input
